0ab0640c33f7985b005fccc8a3ab1e93a8e1e308
Isaac Yriarte <isaacyagi@gmail.com>
2018-10-01 16:43:38 -0500

    Feat: Se obtiene y muestra el token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FCM } from '@ionic-native/fcm';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,13 +11,23 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public fcm: FCM) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.fcm.getToken().then(token => {
+        // Your best bet is to here store the token on the user's profile on the
+        // Firebase database, so that when you want to send notifications to this
+        // specific user you can do it from Cloud Functions.
+        alert(`DEVICE TOKEN=${JSON.stringify(token)}`);
+        console.log(`DEVICE TOKEN=${JSON.stringify(token)}`);
+      });
     });
   }
 }
-
