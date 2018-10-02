19e9e18a7e85923a0082500763d3acb212c84de6
Isaac Yriarte <isaacyagi@gmail.com>
2018-10-01 21:22:06 -0500

    Feat: Se reciben notificaciones push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,13 +21,26 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
       this.fcm.getToken().then(token => {
         // Your best bet is to here store the token on the user's profile on the
         // Firebase database, so that when you want to send notifications to this
         // specific user you can do it from Cloud Functions.
-        alert(`DEVICE TOKEN=${JSON.stringify(token)}`);
         console.log(`DEVICE TOKEN=${JSON.stringify(token)}`);
+        alert(`DEVICE TOKEN=${JSON.stringify(token)}`);
       });
+
+
+      fcm.onNotification().subscribe( data => {
+        if(data.wasTapped){
+          //Notification was received on device tray and tapped by the user.
+          alert(`DATA RECEIVED TAPPED=${JSON.stringify(data)}`);
+        }else{
+          alert(`DATA RECEIVED=${JSON.stringify(data)}`);
+          //Notification was received in foreground. Maybe the user needs to be notified.
+        }
+      });
+
     });
   }
 }
